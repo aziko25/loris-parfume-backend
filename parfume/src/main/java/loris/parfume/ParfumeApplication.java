5a3cb3e7b3674071d4404edd29fb49c992052825
Azizkhuja <azizxoja200425@mail.ru>
2024-07-14 17:49:19 +0500

    initial commit

---
+++ parfume/src/main/java/loris/parfume/ParfumeApplication.java
@@ -0,0 +1,13 @@
+package loris.parfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParfumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParfumeApplication.class, args);
+	}
+
+}
---
+++ parfume/src/test/java/loris/parfume/ParfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package loris.parfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParfumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

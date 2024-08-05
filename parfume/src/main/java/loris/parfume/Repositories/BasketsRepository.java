ecc7593fea00ffdc9b19044c29c9294ac1cc1884
Azizkhuja <azizxoja200425@mail.ru>
2024-08-05 13:50:32 +0500

    deleted unnecessary method in basket repository

--- parfume/src/main/java/loris/parfume/Repositories/BasketsRepository.java
+++ parfume/src/main/java/loris/parfume/Repositories/BasketsRepository.java
@@ -5,8 +5,6 @@ import loris.parfume.Models.Basket_Ids;
 import loris.parfume.Models.Items.Items;
 import loris.parfume.Models.Items.Sizes;
 import loris.parfume.Models.Users;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +13,6 @@ import java.util.List;
 @Repository
 public interface BasketsRepository extends JpaRepository<Basket, Basket_Ids> {
 
-    Page<Basket> findAllByUser(Users user, Pageable pageable);
     List<Basket> findAllByUser(Users user);
 
     void deleteByUserAndItem(Users user, Items item);

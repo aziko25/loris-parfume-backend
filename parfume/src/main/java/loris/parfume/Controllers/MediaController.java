da0fcf17c4c168ce87967c5980f34cbe0ad08825
Azizkhuja <azizxoja200425@mail.ru>
2024-09-12 22:52:23 +0500

    removed image caching

--- parfume/src/main/java/loris/parfume/Controllers/MediaController.java
+++ parfume/src/main/java/loris/parfume/Controllers/MediaController.java
@@ -46,7 +46,6 @@ public class MediaController {
 
             return ResponseEntity.ok()
                     .contentType(MediaType.parseMediaType(contentType))
-                    .header("Cache-Control", "public, max-age=31536000")
                     .body(resource);
 
         } catch (Exception e) {

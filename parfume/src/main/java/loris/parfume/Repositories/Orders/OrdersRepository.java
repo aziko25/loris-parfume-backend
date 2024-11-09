b8fb8309d7ec47bb9349bcf9171900c1629eed0e
Azizkhuja <azizxoja200425@mail.ru>
2024-11-09 12:37:00 +0500

    fixed cjm v2

--- parfume/src/main/java/loris/parfume/Repositories/Orders/OrdersRepository.java
+++ parfume/src/main/java/loris/parfume/Repositories/Orders/OrdersRepository.java
@@ -1,6 +1,5 @@
 package loris.parfume.Repositories.Orders;
 
-import loris.parfume.Models.Branches;
 import loris.parfume.Models.Orders.Orders;
 import loris.parfume.Models.Users;
 import org.springframework.data.domain.Page;
@@ -8,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,6 +15,4 @@ public interface OrdersRepository extends JpaRepository<Orders, Long> {
     Page<Orders> findAllByUser(Users user, Pageable pageable);
 
     Optional<Orders> findByIdAndUser(Long id, Users user);
-
-    List<Orders> findAllByBranch(Branches branch);
 }

3c306901aafb92c2d513fe668305bc60a1f4311d
Azizkhuja <azizxoja200425@mail.ru>
2024-11-30 11:26:41 +0500

    added collections and sizes items list in orders

--- parfume/src/main/java/loris/parfume/DTOs/returnDTOs/Orders_Items_DTO.java
+++ parfume/src/main/java/loris/parfume/DTOs/returnDTOs/Orders_Items_DTO.java
@@ -6,10 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import loris.parfume.Configurations.Serializers.DoubleSerializer;
+import loris.parfume.Models.Items.Collections_Items;
+import loris.parfume.Models.Items.Sizes_Items;
 import loris.parfume.Models.Orders.Orders_Items;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static loris.parfume.Configurations.Serializers.DoubleSerializer.getFormattedPrice;
 
 @Getter
 @Setter
@@ -53,6 +59,9 @@ public class Orders_Items_DTO implements Serializable {
     private String sizeNameRu;
     private String sizeNameEng;
 
+    private List<Map<String, Object>> collectionsItemsList;
+    private List<Map<String, Object>> sizesItemsList;
+
     public Orders_Items_DTO(Orders_Items ordersItem) {
 
         id = ordersItem.getItem().getId();
@@ -94,5 +103,50 @@ public class Orders_Items_DTO implements Serializable {
         sizeNameUz = ordersItem.getSize().getNameUz();
         sizeNameRu = ordersItem.getSize().getNameRu();
         sizeNameEng = ordersItem.getSize().getNameEng();
+
+        collectionsItemsList = new ArrayList<>();
+        if (ordersItem.getItem().getCollectionsItemsList() != null && !ordersItem.getItem().getCollectionsItemsList().isEmpty()) {
+
+            Set<Collections_Items> collectionsItems = new HashSet<>(ordersItem.getItem().getCollectionsItemsList());
+            collectionsItemsList = collectionsItems.stream()
+                    .sorted(Comparator.comparing((Collections_Items pm) -> pm.getCollection().getId())
+                            .thenComparing(pm -> pm.getCollection().getNameUz()))
+                    .map(collection -> {
+
+                        Map<String, Object> map = new LinkedHashMap<>();
+
+                        map.put("collectionId", collection.getCollection().getId());
+                        map.put("collectionSlug", collection.getCollection().getSlug());
+                        map.put("collectionNameUz", collection.getCollection().getNameUz());
+                        map.put("collectionNameRu", collection.getCollection().getNameRu());
+                        map.put("collectionNameEng", collection.getCollection().getNameEng());
+
+                        return map;
+                    }).collect(Collectors.toList());
+        }
+
+        sizesItemsList = new ArrayList<>();
+        if (ordersItem.getItem().getSizesItemsList() != null && !ordersItem.getItem().getSizesItemsList().isEmpty()) {
+
+            Set<Sizes_Items> sizesItems = new HashSet<>(ordersItem.getItem().getSizesItemsList());
+            sizesItemsList = sizesItems.stream()
+                    .sorted(Comparator.comparing((Sizes_Items si) -> si.getSize().getId())
+                            .thenComparing(si -> si.getSize().getNameUz()))
+                    .map(size -> {
+
+                        Map<String, Object> map = new LinkedHashMap<>();
+
+                        map.put("sizeId", size.getSize().getId());
+
+                        map.put("sizeNameUz", size.getSize().getNameUz());
+                        map.put("sizeNameRu", size.getSize().getNameRu());
+                        map.put("sizeNameEng", size.getSize().getNameEng());
+
+                        map.put("price", getFormattedPrice(size.getPrice()));
+                        map.put("discountPercent", size.getDiscountPercent());
+
+                        return map;
+                    }).collect(Collectors.toList());
+        }
     }
 }
